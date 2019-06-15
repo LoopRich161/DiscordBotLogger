6417edc0285a70cc2e04307432b1aa7dfe9edd39
LoopRich161 <kirill-shakhmatov@mail.ru>
2019-06-15 08:54:58 +0300

    Reg command "toggle"

--- src/main/java/ru/frostdelta/discord/BotCommand.java
+++ src/main/java/ru/frostdelta/discord/BotCommand.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import ru.looprich.discordlogger.Core;
 import ru.looprich.discordlogger.events.EventHandlers;
 import ru.looprich.discordlogger.modules.DiscordBot;
@@ -11,38 +12,49 @@ import ru.looprich.discordlogger.modules.DiscordBot;
 public class BotCommand implements CommandExecutor {
 
     public static void reg(){
-        Core.getInstance().getCommand("bot").setExecutor(new BotCommand());
+        BotCommand botCommand = new BotCommand();
+        Core.getInstance().getCommand("bot").setExecutor(botCommand);
+        Core.getInstance().getCommand("toggle").setExecutor(botCommand);
+
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player){
+            sender.sendMessage("Only for console!");
+            return true;
+        }
         if(command.getName().equalsIgnoreCase("toggle") && args.length == 1){
             if (DiscordBot.isLocalEnabled()) {
                 DiscordBot.setLocalEnabled(false);
-            } else DiscordBot.setLocalEnabled(true);
-            EventHandlers.sendMessage("Чат переключен!");
+                EventHandlers.sendMessage("Локальный чат отключен!");
+            } else {
+                DiscordBot.setLocalEnabled(true);
+                EventHandlers.sendMessage("Локальный чат включен!");
+            }
             return true;
-        }
+        }else sender.sendMessage(ChatColor.RED + "Command usage: " + ChatColor.GOLD + "/toggle <true/false>");
+
         if(command.getName().equalsIgnoreCase("bot") && args.length == 1){
             switch (args[0]){
                 case "enable":
                     if(!DiscordBot.getBot().isEnabled()) {
                         Core.getInstance().loadDiscordBot();
-                        System.out.println("Bot successful enabled!");
-                    } else System.out.println("Bot already enabled!");
+                        Core.getInstance().getLogger().info("Bot successful enabled!");
+                    } else Core.getInstance().getLogger().info("Bot already enabled!");
                     break;
                 case "disable":
                     DiscordBot.getBot().getJDA().shutdownNow();
-                    System.out.println("Bot successful disabled!");
+                    Core.getInstance().getLogger().info("Bot successful disabled!");
                     break;
                 case "restart":
                     if(DiscordBot.getBot().isEnabled()) {
                         DiscordBot.getBot().getJDA().shutdownNow();
                         Core.getInstance().loadDiscordBot();
-                        System.out.println("Bot successful restarted!");
+                        Core.getInstance().getLogger().info("Bot successful restarted!");
                     } else {
                         Core.getInstance().loadDiscordBot();
-                        System.out.println("Bot successful restarted!");
+                        Core.getInstance().getLogger().info("Bot successful restarted!");
                     }
                     break;
             }
