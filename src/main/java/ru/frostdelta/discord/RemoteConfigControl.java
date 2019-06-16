b6f6b90c56061adab1eff30c2bc2e2819129792f
LoopRich161 <kirill-shakhmatov@mail.ru>
2019-06-16 21:50:33 +0300

    load: STARTUP

--- src/main/java/ru/frostdelta/discord/RemoteConfigControl.java
+++ src/main/java/ru/frostdelta/discord/RemoteConfigControl.java
@@ -1,9 +1,15 @@
 package ru.frostdelta.discord;
 
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class RemoteConfigControl extends ListenerAdapter {
+
+    public void onGuildMessageReceivedEvent(GuildMessageReceivedEvent event){
+        String [] args = event.getMessage().getContentRaw().split(" ");
+
+    }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
