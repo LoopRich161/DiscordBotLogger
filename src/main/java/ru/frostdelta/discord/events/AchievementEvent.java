531e71add42b91393b7f9d5077843cedcc847123
FrostDelta123 <pawel.soko2012@yandex.ru>
2019-06-16 22:20:52 +0300

    Achievement output fix

--- src/main/java/ru/frostdelta/discord/events/AchievementEvent.java
+++ src/main/java/ru/frostdelta/discord/events/AchievementEvent.java
@@ -4,16 +4,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerAchievementAwardedEvent;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 
 import static ru.looprich.discordlogger.modules.DiscordBot.sendMessage;
 
 public class AchievementEvent implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
-    void achivement(PlayerAchievementAwardedEvent event) {
+    void achivement(PlayerAdvancementDoneEvent event) {
         Player player = event.getPlayer();
-        sendMessage(player.getName() + " has made the achievement " + event.getAchievement().name());
+        sendMessage(player.getName() + " has made the achievement " + event.getAdvancement().toString());
     }
 
 }
