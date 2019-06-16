846bb0a2eb13d7c78bffe5915e6b8e8e8f43c5a2
FrostDelta123 <pawel.soko2012@yandex.ru>
2019-06-16 22:47:01 +0300

    ArrayIndexOutOfBoundsException

--- src/main/java/ru/frostdelta/discord/RemoteConfigControl.java
+++ src/main/java/ru/frostdelta/discord/RemoteConfigControl.java
@@ -12,7 +12,7 @@ public class RemoteConfigControl extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
 
         String [] args = event.getMessage().getContentRaw().split(" ");
-        if (args[1].equalsIgnoreCase("help")){
+        if (args.length == 2 && args[1].equalsIgnoreCase("help")){
             EmbedBuilder info = new EmbedBuilder();
             info.setTitle("Помощь");
             info.setDescription("Просмотрев данную информацию станет легче что-то там.");
