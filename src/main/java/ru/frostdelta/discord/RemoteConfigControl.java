bc15a516fe804ffb71e55e78f86c42c55cf81f47
LoopRich161 <kirill-shakhmatov@mail.ru>
2019-06-16 23:20:21 +0300

    RemoteConfigControl

--- src/main/java/ru/frostdelta/discord/RemoteConfigControl.java
+++ src/main/java/ru/frostdelta/discord/RemoteConfigControl.java
@@ -14,7 +14,8 @@ public class RemoteConfigControl extends ListenerAdapter {
         if (args.length == 0 || !(args[0].equalsIgnoreCase(DiscordBot.prefix))) {
             return;
         }
-        if (args.length == 2 && args[1].equalsIgnoreCase("help")) {
+        String command = args[1];
+        if (args.length == 2 && command.equalsIgnoreCase("help")) {
             EmbedBuilder info = new EmbedBuilder();
             info.setTitle("Помощь");
             info.setDescription("Просмотрев данную информацию станет легче что-то там.");
@@ -23,7 +24,45 @@ public class RemoteConfigControl extends ListenerAdapter {
 
             event.getChannel().sendTyping().queue();
             event.getChannel().sendMessage(info.build()).queue();
+            return;
         }
+        if (command.equalsIgnoreCase("toggle")&& args.length == 2) {
+            if (DiscordBot.isLocalEnabled()) {
+                DiscordBot.setLocalEnabled(false);
+                DiscordBot.sendImportantMessage("Локальный чат отключен!");
+                DiscordLogger.getInstance().getConfig().set("local-chat", false);
+            } else {
+                DiscordBot.setLocalEnabled(true);
+                DiscordBot.sendImportantMessage("Локальный чат включен!");
+                DiscordLogger.getInstance().getConfig().set("local-chat", true);
+            }
+            return;
+        }
+
+        if (command.equalsIgnoreCase("bot") && args.length == 3) {
+            switch (args[2]) {
+                case "reload":
+                    DiscordLogger.getInstance().reloadConfig();
+                    event.getChannel().sendMessage("Bot already enabled!");
+                    break;
+                case "disable":
+                    DiscordBot.shutdown();
+                    event.getChannel().sendMessage("Bot successful disabled!");
+                    break;
+                case "restart":
+                    if (DiscordBot.isEnabled()) {
+                        DiscordBot.shutdown();
+                        DiscordLogger.getInstance().loadDiscordBot();
+                        event.getChannel().sendMessage("Bot successful restarted!");
+                    } else {
+                        DiscordLogger.getInstance().loadDiscordBot();
+                        event.getChannel().sendMessage("Bot successful restarted!");
+                    }
+                    break;
+            }
+        } else event.getChannel().sendMessage("Command usage: /bot <disable/restart>");
+
+
     }
 
 }
