39c1fc87156568487178be48a3350bb61be37398
LoopRich161 <kirill-shakhmatov@mail.ru>
2019-07-01 23:09:13 +0300

    Merge command developers and add prefix for command "developers" Add command "~bot online" Add info for developers

--- src/main/java/ru/frostdelta/discord/RemoteConfigControl.java
+++ src/main/java/ru/frostdelta/discord/RemoteConfigControl.java
@@ -3,10 +3,14 @@ package ru.frostdelta.discord;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import ru.looprich.discordlogger.DiscordLogger;
 import ru.looprich.discordlogger.modules.DiscordBot;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static ru.looprich.discordlogger.modules.DiscordBot.sendImportantMessage;
 
@@ -26,28 +30,17 @@ public class RemoteConfigControl extends ListenerAdapter {
             EmbedBuilder info = new EmbedBuilder();
             info.setTitle("Помощь");
             info.setDescription("Доступные команды:\n" +
-                    "~toggle - *переключение локального чата.*\n" +
+                    "~bot toggle - *переключение локального чата.*\n" +
                     "~bot reload - *перезагрузка конфига.*\n" +
                     "~bot disable - *выключение бота.*\n" +
                     "~bot restart - *перезагрузка бота.*\n" +
-                    "~developers - *информация о разработчиках.*");
+                    "~bot developers - *информация о разработчиках.*\n" +
+                    "~bot online - *просмотреть кто находится на сервере.");
             info.addField("Создатели", Arrays.toString(DiscordLogger.getInstance().getDescription().getAuthors().toArray()), false);
             info.setColor(0x008000);
 
             event.getChannel().sendTyping().queue();
             event.getChannel().sendMessage(info.build()).queue();
-            return;
-        }
-
-        if (command.equalsIgnoreCase(DiscordBot.prefix + "developers")) {
-            EmbedBuilder developers = new EmbedBuilder();
-            developers.setTitle("Разработчики");
-            developers.setDescription("LoopRich161 - *.*\n" +
-                    "FrostDelta123 - *.*");
-            developers.setColor(0x0000ff);
-
-            event.getChannel().sendTyping().queue();
-            event.getChannel().sendMessage(developers.build()).queue();
             return;
         }
 
@@ -63,7 +56,7 @@ public class RemoteConfigControl extends ListenerAdapter {
                 sendImportantMessage("Локальный чат включен! (" + who + ")");
                 DiscordLogger.getInstance().getConfig().set("local-chat", true);
             }
-            DiscordLogger.getInstance().getLogger().info("<" + who + "> issued server command: ~toggle");
+            DiscordLogger.getInstance().getLogger().info("<" + who + "> issued discord command: ~toggle");
             return;
         }
 
@@ -75,7 +68,7 @@ public class RemoteConfigControl extends ListenerAdapter {
                     break;
                 case "disable":
                     sendImportantMessage("Я выключился!");
-                    DiscordLogger.getInstance().getLogger().info("<" + who + "> issued server command: ~bot disable");
+                    DiscordLogger.getInstance().getLogger().info("<" + who + "> issued discord command: ~bot disable");
                     DiscordBot.shutdown();
                     break;
                 case "restart":
@@ -86,12 +79,33 @@ public class RemoteConfigControl extends ListenerAdapter {
                     } else {
                         DiscordLogger.getInstance().loadDiscordBot();
                     }
-                    DiscordLogger.getInstance().getLogger().info("<" + who + "> issued server command: ~bot restart");
+                    DiscordLogger.getInstance().getLogger().info("<" + who + "> issued discord command: ~bot restart");
                     sendImportantMessage("Я перезагрузился! (" + who + ")");
+                    break;
+                case "developers":
+                    EmbedBuilder developers = new EmbedBuilder();
+                    developers.setTitle("Разработчики");
+                    developers.setDescription("LoopRich161 - *создатель плагина.*\n" +
+                            "FrostDelta123 - *человек-идея, а так же фиксящий ошибки и исправляющий костыли.*");
+                    developers.setColor(0x0000ff);
 
+                    event.getChannel().sendTyping().queue();
+                    event.getChannel().sendMessage(developers.build()).queue();
+                    break;
+                case "online":
+                    EmbedBuilder online = new EmbedBuilder();
+                    List<String> onlinePlayers = new ArrayList<>();
+                    for (Player player : Bukkit.getServer().getOnlinePlayers()) onlinePlayers.add(player.getName());
+                    online.setTitle("Онлайн сервера");
+                    online.setDescription(Bukkit.getServer().getOnlinePlayers().size() + "/" + Bukkit.getServer().getMaxPlayers()
+                            + "\nИгроки: " + onlinePlayers.toString().replace("[", "").replace("]", ""));
+                    online.setColor(0xFFFF00);
+
+                    event.getChannel().sendTyping().queue();
+                    event.getChannel().sendMessage(online.build()).queue();
                     break;
             }
-        } else event.getChannel().sendMessage("Доступные команды: ~bot <disable/restart>");
+        } else event.getChannel().sendMessage("Доступные команды: ~bot <disable/restart/developers/online>");
 
     }
 
