56b2f4c6f206e9a78ccb55dfea6725bc9417b311
FrostDelta123 <pawel.soko2012@yandex.ru>
2019-07-04 01:58:13 +0300

    Config refactor

--- src/main/java/ru/frostdelta/discord/BotCommandAdapter.java
+++ src/main/java/ru/frostdelta/discord/BotCommandAdapter.java
@@ -2,12 +2,24 @@ package ru.frostdelta.discord;
 
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import ru.looprich.discordlogger.modules.DiscordBot;
 
 public class BotCommandAdapter extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        if (DiscordBot.commandOnlyOneChannel)
+            if (!event.getTextChannel().getId().equalsIgnoreCase(DiscordBot.getLoggerChannel().getId()) && event.getTextChannel() != null)
+                return;
 
+        String[] args = event.getMessage().getContentRaw().split(" ");
+        if (args.length == 0) {
+            return;
+        }
+        String command = args[0];
+        if(command.equalsIgnoreCase(DiscordBot.prefix + "command")){
+
+        }
     }
 
 }
