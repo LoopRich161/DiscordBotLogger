93a828190921b862b5741cc5ae920dc8c1a801e5
FrostDelta123 <pawel.soko2012@yandex.ru>
2019-07-09 19:53:52 +0300

    FakePlayerCommandSender added

--- src/main/java/ru/frostdelta/discord/FakePlayerCommandSender.java
+++ src/main/java/ru/frostdelta/discord/FakePlayerCommandSender.java
@@ -1,8 +1,11 @@
 package ru.frostdelta.discord;
 
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 
+import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.permissions.PermissionAttachmentInfo;
@@ -12,25 +15,43 @@ import java.util.Set;
 
 public class FakePlayerCommandSender implements CommandSender {
 
+    private OfflinePlayer offlinePlayer;
+    private Player player;
+    private boolean isOnline;
+
+    public FakePlayerCommandSender(String name){
+        offlinePlayer = Bukkit.getOfflinePlayer(name);
+        player = offlinePlayer.getPlayer();
+        isOnline = player != null;
+    }
 
     @Override
     public void sendMessage(String message) {
-
+        if(isOnline){
+            player.sendMessage(message);
+        }
     }
 
     @Override
     public void sendMessage(String[] messages) {
-
+        if(isOnline){
+            for(String message : messages){
+                player.sendMessage(message);
+            }
+        }
     }
 
     @Override
     public Server getServer() {
-        return null;
+        return Bukkit.getServer();
     }
 
     @Override
     public String getName() {
-        return null;
+        if(isOnline){
+            return player.getName();
+        }
+        return offlinePlayer.getName();
     }
 
     @Override
@@ -90,11 +111,15 @@ public class FakePlayerCommandSender implements CommandSender {
 
     @Override
     public boolean isOp() {
-        return false;
+        if(isOnline){
+            return player.isOp();
+        }else return offlinePlayer.isOp();
     }
 
     @Override
     public void setOp(boolean value) {
-
+        if(isOnline){
+            player.setOp(value);
+        }else offlinePlayer.setOp(value);
     }
 }
