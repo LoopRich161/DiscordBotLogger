b0043a9d2b6e2424feaeecc7bb67c80e78bc02e8
LoopRich161 <kirill-shakhmatov@mail.ru>
2019-07-10 08:25:10 +0300

    Merge method from DiscordBot to Util

--- src/main/java/ru/frostdelta/discord/Util.java
+++ src/main/java/ru/frostdelta/discord/Util.java
@@ -7,15 +7,42 @@ import java.util.List;
 
 public class Util {
 
-    public static String removeCodeColors(String message){
+    public static String removeCodeColors(String message) {
         return ChatColor.stripColor(message);
     }
 
-    public static List<String> removeCodeColors(String[] messages){
+    public static List<String> removeCodeColors(String[] messages) {
         List<String> message = new ArrayList<>();
-        for(String msg : messages){
+        for (String msg : messages)
             message.add(ChatColor.stripColor(msg));
-        }
         return message;
     }
+
+    public static String cancelFormatMessage(String message) {
+        String[] array = message.split(" ");
+        StringBuilder msg = new StringBuilder();
+        int pos1, pos2, difference;
+        for (int i = 0; i <= array.length - 1; i++) {
+            StringBuilder buff = new StringBuilder(" " + array[i]);
+
+            pos1 = buff.toString().indexOf('_');
+            pos2 = buff.toString().lastIndexOf('_');
+            difference = Math.max(pos1, pos2) - Math.min(pos1, pos2);
+            if (pos1 != pos2 && difference != 1) buff.insert(pos1, "\\");
+
+            pos1 = buff.toString().indexOf('*');
+            pos2 = buff.toString().lastIndexOf('*');
+            difference = Math.max(pos1, pos2) - Math.min(pos1, pos2);
+            if (pos1 != pos2 && difference != 1) buff.insert(pos1, "\\");
+
+            pos1 = buff.toString().indexOf('~');
+            pos2 = buff.toString().lastIndexOf('~');
+            difference = Math.max(pos1, pos2) - Math.min(pos1, pos2);
+            if (pos1 != pos2 && difference != 1) buff.insert(pos1, " \\");
+
+            msg.append(buff.toString());
+        }
+        return msg.toString();
+    }
+
 }
