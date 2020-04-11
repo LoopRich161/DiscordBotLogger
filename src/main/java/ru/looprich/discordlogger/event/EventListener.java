e4c11ba1f13b59c252383d0cc90017d93cb85bb0
LoopRich161 <looprich161@mail.ru>
2020-04-11 14:56:23 +0300

    Now achievements are completed

--- src/main/java/ru/looprich/discordlogger/event/EventListener.java
+++ src/main/java/ru/looprich/discordlogger/event/EventListener.java
@@ -77,7 +77,7 @@ public class EventListener implements Listener {
                 .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
                 .collect(Collectors.joining(" "));
 
-        DiscordBot.sendMessageChannel(event.getPlayer().getName() + " has made the new achievement ["+advancementName+"]");
+        DiscordBot.sendMessageChannel(event.getPlayer().getName() + " has made the achievement ["+advancementName+"]");
     }
 
 }
