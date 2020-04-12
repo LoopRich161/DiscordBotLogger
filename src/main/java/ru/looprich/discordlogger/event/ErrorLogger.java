839e9d34dc88b349bdc849a531da5c97f5be8e64
LoopRich161 <looprich161@mail.ru>
2020-04-13 01:14:18 +0300

    Write ErrorLogger.class Wait test this

--- src/main/java/ru/looprich/discordlogger/event/ErrorLogger.java
+++ src/main/java/ru/looprich/discordlogger/event/ErrorLogger.java
@@ -1,11 +1,23 @@
 package ru.looprich.discordlogger.event;
 
-import org.bukkit.Bukkit;
 
-public class ErrorLogger {
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
 
-    public void test(){
-        Bukkit.getServer().getLogger().getLevel();
+public class ErrorLogger extends AbstractAppender {
+    public ErrorLogger(Appender appender) {
+        super(appender.getName(), null, appender.getLayout(), false);
     }
+
+    @Override
+    public void append(LogEvent logEvent) {
+        //wait test
+        System.out.println("logEvent.getLevel(): " + logEvent.getLevel());
+        System.out.println("logEvent.getMessage(): " + logEvent.getMessage());
+        System.out.println("logEvent.getThrown().getMessage(): " + logEvent.getThrown().getMessage());
+        System.out.println("logEvent.getContextData().toString():" + logEvent.getContextData().toString());
+    }
+
 
 }
