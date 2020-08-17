a67eb06d76aba464bf70e5bca17705cbf7879a25
kirill <looprich161@mail.ru>
2020-08-17 12:39:37 +0300

    Removed unnecessary code that was needed for the test

--- src/main/java/ru/frostdelta/discord/Util.java
+++ src/main/java/ru/frostdelta/discord/Util.java
@@ -1,16 +1,13 @@
 package ru.frostdelta.discord;
 
 import net.dv8tion.jda.api.utils.MarkdownSanitizer;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import ru.looprich.discordlogger.DiscordLogger;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class Util {
 
@@ -51,84 +48,6 @@ public class Util {
 
     public static String cancelFormatMessage(String message) {
         return MarkdownSanitizer.escape(message);
-    }
-
-    public static Location getLocationOfflinePlayer(UUID uuid) {
-//        File playerData = null;
-//        World worldPlayer = null;
-//        for (World world : Bukkit.getServer().getWorlds()) {
-//            File tempPlayerdata = new File(world.getWorldFolder(), "playerdata");
-//            for (File playerd : tempPlayerdata.listFiles()) {
-//                if (playerd.getName().startsWith(uuid.toString())) {
-//                    playerData = playerd;
-//                    worldPlayer = world;
-//                    break;
-//                }
-//            }
-//        }
-//
-//
-//        NBTInputStream NBTIStream = null;
-//        try {
-//            NBTIStream = new NBTInputStream(new FileInputStream(playerData));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        CompoundTag rootCompoundTag = null;
-//        try {
-//            rootCompoundTag = (CompoundTag) NBTIStream.readTag();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        ListTag position = (ListTag) rootCompoundTag.getValue().get("Pos");
-//        ListTag rotation = (ListTag) rootCompoundTag.getValue().get("Rotation");
-        //юзать вместо Pos и Rotation AABB и брать значения от 0 до 2 для позиции, а яв и питч ставить левые
-        //System.out.println("sizePos: "+position.getValue().size()+", array: "+Arrays.toString(position.getValue().toArray()));
-        //System.out.println("sizeRotation: "+rotation.getValue().size()+", array: "+Arrays.toString(rotation.getValue().toArray()));
-//        double x = (double) position.getValue().get(0).getValue();
-//        double y = (double) position.getValue().get(1).getValue();
-//        double z = (double) position.getValue().get(2).getValue();
-//        float yaw = (float) rotation.getValue().get(0).getValue();
-//        float pitch = (float) rotation.getValue().get(1).getValue();
-        return new Location(Bukkit.getWorlds().get(0), 9, 12, 23, 65, 123);
-    }
-
-    public static Location getLocationOfflinePlayer(String playerName) {
-//        UUID uuid = Bukkit.getOfflinePlayer(playerName).getUniqueId();
-//        File playerData = null;
-//        World worldPlayer = null;
-//        for (World world : Bukkit.getWorlds()) {
-//            File tempPlayerdata = new File(world.getWorldFolder(), "playerdata");
-//            for (File playerd : tempPlayerdata.listFiles()) {
-//                if (playerd.getName().startsWith(uuid.toString())) {
-//                    playerData = playerd;
-//                    worldPlayer = world;
-//                    break;
-//                }
-//            }
-//        }
-//
-//        NBTInputStream NBTIStream = null;
-//        try {
-//            NBTIStream = new NBTInputStream(new FileInputStream(playerData));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        CompoundTag rootCompoundTag = null;
-//        try {
-//            rootCompoundTag = (CompoundTag) NBTIStream.readTag();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        ListTag position = (ListTag) rootCompoundTag.getValue().get("Pos");
-//        ListTag rotation = (ListTag) rootCompoundTag.getValue().get("Rotation");
-//
-//        double x = (double) position.getValue().get(0).getValue();
-//        double y = (double) position.getValue().get(1).getValue();
-//        double z = (double) position.getValue().get(2).getValue();
-//        float yaw = (float) rotation.getValue().get(0).getValue();
-//        float pitch = (float) rotation.getValue().get(1).getValue();
-        return new Location(Bukkit.getWorlds().get(0), 9, 12, 23, 65, 123);
     }
 
     public static boolean isDate(String date) {
